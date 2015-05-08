7d47ba3613aa9de97d0e0322bbbfefa9a377a5e5
Nathan Gallup <ngallup@bu.edu>
2015-05-07 19:08:57 -0700

    The ridft method has been populated and given basic error handling, however the rollback feature is still unimplemented

---
+++ test/script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+sys.path.append('../')
+
+import turboclass
+
+test = turboclass.Turboclass()
+
+test.ridft()
+
+#test.rdgrad()
+
--- turboclass.py
+++ turboclass.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 __author__= 'Nathan Gallup'
 '''
-===========================
+==============================================================================
 turboclass.py
 
-For running Turbomole with frozen internal coordinate optimizations, but could be generally used for Turbomole optimizations involving internal coordinates.  Attempts to automatically handle the problem of Turbomole sometimes producing linearly dependent internal redundant coordinates during optimization by switching between optimizing with the frozen cartesian atoms and frozen internals.  Also attempts to continue the optimization of currently running internal coordinate calculations via autosubmission.  This script is entirely intended to be a single user-executed script that then runs a frozen internal coordinate calculation to its completion, often days later.
+For running Turbomole with frozen internal coordinate optimizations, but could
+be generally used for Turbomole optimizations involving internal coordinates.  
+Attempts to automatically handle the problem of Turbomole sometimes producing
+linearly dependent internal redundant coordinates during optimization by
+switching between optimizing with the frozen cartesian atoms and frozen
+internals.  Also attempts to continue the optimization of currently running
+internal coordinate calculations via autosubmission.  This script is entirely
+intended to be a single user-executed script that then runs a frozen internal
+coordinate calculation to its completion, often days later.
 
 March 2015, UCLA
-===========================
+==============================================================================
 '''
 
-import os, sys, re, optparse
+import os, sys, re, optparse, subprocess
 import freeze, unfreeze
 
 # For easy submission, FINISH LATER.  LONG TERM.
@@ -50,19 +58,51 @@ class Turboclass(object):
 		self.homeDir = os.getcwd()
 		
 		if os.path.exists(os.path.join(self.turboDir, 'turbohistory.txt')) == False:
-			
+			temp = open(os.path.join(self.turboDir, 'turbohistory.txt'), 'w')
+			temp.close()
 
 	# For running a simple ridft.  Rollback variable implemented for easy recall
 	# of an energy for a particular geometry.  Rollback feature could be
 	# implemented here or in a dedicated rollback function
 	def ridft(self, rollback=None):
-		pass
-	
+
+		# Implement some other time
+		if rollback != None:
+			pass
+		
+		p = subprocess.Popen("ridft", shell=True, cwd=self.turboDir, 
+									stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		out = p.stdout.read()
+		print out
+
+		# Error several times before terminating
+		tries = 1
+		numtries = 2
+		while "ridft ended abnormally" in out:
+			if tries > numtries:
+				print "ridft has failed for unknown reasons and could not be " \
+						"recovered.  Check that the setup is alright"
+				sys.exit(1)
+
+			print "Abnormal termination detected.  Attempting actual -r"
+			actual = subprocess.Popen("actual -r", shell=True, cwd=self.turboDir,
+									stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			out = actual.stdout.read()
+			print out
+			
+			print "Re-attempting ridft"
+			p = subprocess.Popen("ridft", shell=True, cwd=self.turboDir,
+								stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			out = p.stdout.read()
+			print out
+			
+			tries += 1
+
 	# For running a simple rdgrad.  Rollback variable implemented for easy 
 	# recall of a gradient for a particular geometry.  Rollback feature could be
 	# implemented here or in a dedicated rollback function
 	def rdgrad(self, rollback=None):
-		pass
+		os.system("rdgrad")
 	
 	# For running jobex.  Rollback vairable implemented for easy recall of a 
 	# particular geometry.  Rollback feature could be implemented
@@ -77,5 +117,5 @@ class Turboclass(object):
 	def constrained_int_ts(self, rollback=None, otherflags=None):
 		pass
 	
-	def rollback(self)
+	def rollback(self):
 		pass
